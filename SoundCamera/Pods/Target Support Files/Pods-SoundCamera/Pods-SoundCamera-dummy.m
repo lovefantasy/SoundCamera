5d3377232b6f20508c3f95f290c038919c47d7a0
Cytus Chang <cytus.chang@ulsee.com>
2017-08-02 16:15:57 +0800

    Initial wireup.

---
+++ SoundCamera/Pods/Target Support Files/Pods-SoundCamera/Pods-SoundCamera-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SoundCamera : NSObject
+@end
+@implementation PodsDummy_Pods_SoundCamera
+@end
---
+++ SoundCamera/SoundCamera/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SoundCamera
+//
+//  Created by Chih-Kuang Chang on 2017/8/2.
+//  Copyright © 2017年 Cytus Chang. All rights reserved.
+//
+
+import UIKit
+import AudioKit
+import GPUImage
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
